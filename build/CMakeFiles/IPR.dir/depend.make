7782cf6318bd4df724bd04e86e42b3322623a474
SaeedMogharian <moghariansaeed@gmail.com>
2024-11-24 13:45:36 +0000

    cmake done

---
+++ build/CMakeFiles/IPR.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IPR.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/transparent-node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transparent-node.dir/src/wire.cpp.o"
+  "CMakeFiles/transparent-node.dir/src/wire.cpp.o.d"
+  "transparent-node"
+  "transparent-node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transparent-node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
